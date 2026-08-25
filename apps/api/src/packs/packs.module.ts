85e2389653d9083909ed3241571eb0f3869910ff
Miguel Lupani <mlupani2@gmail.com>
2026-08-25 14:54:45 -0300

    fix api

--- apps/api/src/packs/packs.module.ts
+++ apps/api/src/packs/packs.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BusinessesModule } from '../businesses/businesses.module';
 import { PackBalanceService } from './pack-balance.service';
 import { PacksAdminController } from './packs-admin.controller';
 
 @Module({
+  imports: [BusinessesModule],
   controllers: [PacksAdminController],
   providers: [PackBalanceService],
   exports: [PackBalanceService],
